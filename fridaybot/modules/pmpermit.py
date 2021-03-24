8ec3a14af7bd327d574ac8765b90789639e44ddc
liandab <81314748+liandab@users.noreply.github.com>
2021-03-24 22:54:23 +0530

    Update pmpermit.py

--- fridaybot/modules/pmpermit.py
+++ fridaybot/modules/pmpermit.py
@@ -33,7 +33,7 @@ USER_BOT_WARN_ZERO = "You Have Attempted To Spam Masters Inbox So Inorder To Avo
 devs_id = [1263617196, 573738900, 1315076555, 1141839926]
 
 USER_BOT_NO_WARN = (
-    "**Hello, This is Friday PM Protection Service ⚠️**\n\n"
+    "**Hello, This is Friday PM Protection Service ⚠️. Hello. My master is busy soul unlike you idle ghost.Once he is online then he will message you.Unless then I don't want you should spam or do nothing non sense thing. My master don't like it.Wait calmly till my beloved master comes online.If you did any any of misconduct I have to block you.Thank you.Peace out.**\n\n"
     f"`My Master {bot.me.first_name} is Busy Right Now !` \n"
     "**I Request You To Choose A Reason You Have Came For** 👀 \n\n"
     f"**{CUSTOM_MIDDLE_PMP}**"
